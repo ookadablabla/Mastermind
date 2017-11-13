f53e870aac19aee9d66e14a2c14b417ee993f7cc
Quinten Hutchison <Quinten@case.edu>
2017-11-13 13:00:43 -0500

    Updated rules to include the valid digits. It now shows the correct answer when you lose. Updated my brackets per Vicki's feedback on proper c# formatting.

--- Mastermind/Program.cs
+++ Mastermind/Program.cs
@@ -13,31 +13,35 @@ namespace Mastermind
         private static Boolean userHasWon = false;
         private static String currentGuess;
 
-        public static void Main(String[] args) {
+        public static void Main(String[] args)
+        {
             NewGame();
 
             //allow the user to play another game
             ConsoleKey userInput;
-            do {
+            do
+            {
                 //prompt them if they want to play again
-                UI.ClearConsoleLine(UI.PromptLine + 1);
+                //UI.ClearConsoleLine(UI.PromptLine + 1);
                 Console.Write("Play Again? (Y/N): ");
 
                 //check if they said yes
                 userInput = Console.ReadKey().Key;
-                if (userInput == ConsoleKey.Y) {
+                if (userInput == ConsoleKey.Y)
+                {
                     Console.Clear();
                     NewGame();
                 }
 
-            //loop until they say no
+                //loop until they say no
             } while (userInput != ConsoleKey.N);
 
 
         }
 
         //resets the variables and starts a new game of Mastermind
-        private static void NewGame() {
+        private static void NewGame()
+        {
             //initialize/reset all of the variables
             guesses = 0;
             RandomizeCode();
@@ -48,24 +52,26 @@ namespace Mastermind
             UI.WriteInColor(UI.title, ConsoleColor.Green);
             Console.WriteLine(UI.instructions);
             Console.WriteLine(UI.scoreboardHeader);
-            
+
 
             //keep prompting the user for a guess
-            while (!userHasWon && guesses < Settings.MAX_GUESSES) {
-                //prompt for a new guess
+            while (!userHasWon && guesses < Settings.MAX_GUESSES)
+            {
+                //write prompt
                 UI.ClearConsoleLine(UI.PromptLine);
                 Console.Write(UI.promptMessage);
 
                 currentGuess = Console.ReadLine();
 
                 //if the user input a valid guess
-                if (IsValidGuess(currentGuess)) {
+                if (IsValidGuess(currentGuess))
+                {
                     guesses++;
 
                     //clear the prompt and error from the previous question
                     UI.ClearConsoleLine(UI.ErrorLine - 1);
                     UI.ClearConsoleLine(UI.PromptLine - 1);
-                    
+
                     //add the text to the scoreboard
                     Console.SetCursorPosition(0, UI.ScoreboardLine);
                     Console.WriteLine(UI.ScoreBoardText);
@@ -73,8 +79,10 @@ namespace Mastermind
                     //check if the user won
                     userHasWon = currentGuess.Equals(code);
 
-                //otherwise write an error
-                } else {
+                    //otherwise write an error
+                }
+                else
+                {
                     Console.SetCursorPosition(0, UI.ErrorLine);
                     UI.WriteInColor(UI.errorMessage, ConsoleColor.Red);
                 }
@@ -82,21 +90,27 @@ namespace Mastermind
 
             //clear out the current message, and write the win/lose message
             UI.ClearConsoleLine(UI.PromptLine);
-            if(userHasWon) {
+            if (userHasWon)
+            {
                 Console.WriteLine(UI.winMessage);
-            } else {
+            }
+            else
+            {
                 Console.WriteLine(UI.loseMessage);
+                Console.WriteLine("Solution was: " + code);
             }
 
         }
 
         //set the current solution to be a new random valid code
-        private static void RandomizeCode() {
+        private static void RandomizeCode()
+        {
             StringBuilder newCode = new StringBuilder();
             Random random = new Random();
 
             //build up the code grabbing random valid chars
-            for (int i = 0; i < Settings.CODE_LENGTH; i++) {
+            for (int i = 0; i < Settings.CODE_LENGTH; i++)
+            {
                 newCode.Append(Settings.VALID_CHARS[random.Next(Settings.VALID_CHARS.Length)]);
             }
 
@@ -104,15 +118,19 @@ namespace Mastermind
         }
 
         //checks to make sure the guess only contains valid characters
-        private static bool IsValidGuess(String guess)  {
+        private static bool IsValidGuess(String guess)
+        {
             //check the length
-            if(guess.Length != Settings.CODE_LENGTH) {
+            if (guess.Length != Settings.CODE_LENGTH)
+            {
                 return false;
             }
 
             //itterate through the guess individually validating every character
-            foreach(char c in guess) {
-                if (!Settings.VALID_CHARS.Contains(c)) {
+            foreach (char c in guess)
+            {
+                if (!Settings.VALID_CHARS.Contains(c))
+                {
                     return false;
                 }
             }
@@ -122,23 +140,28 @@ namespace Mastermind
         }
 
         //checks the guess against the correct code, and returns the resulting score
-        private static String ScoreFromGuess(String guess) {
+        private static String ScoreFromGuess(String guess)
+        {
             List<char> matching = new List<char>();
             List<char> wrongPosition = new List<char>();
             char currentChar;
 
             //check the guess against the correct code
-            for (int i=0; i<guess.Length; i++) {
+            for (int i = 0; i < guess.Length; i++)
+            {
                 currentChar = guess[i];
 
                 //first check if the code has the character at all
-                if (code.Contains(currentChar)) {
+                if (code.Contains(currentChar))
+                {
                     //if it does, check to see if the positions match
-                    if(code[i].Equals(currentChar)) {
+                    if (code[i].Equals(currentChar))
+                    {
                         matching.Add(currentChar);
-
+                    }
                     //if they don't, add them to the wrong position list
-                    } else {
+                    else
+                    {
                         wrongPosition.Add(currentChar);
                     }
 
@@ -155,7 +178,7 @@ namespace Mastermind
 
             return score.ToString();
 
-        }        
+        }
 
         private static class UI
         {
@@ -167,18 +190,19 @@ namespace Mastermind
                                                     " digit code consisting only of the characters: " +
                                                     String.Join("", Settings.VALID_CHARS) + "\n";
 
-            public static readonly String title =   " __  __           _                      _           _ \n" +
+            public static readonly String title = " __  __           _                      _           _ \n" +
                                                     "|  \\/  | __ _ ___| |_ ___ _ __ _ __ ___ (_)_ __   __| |\n" +
                                                     "| |\\/| |/ _` / __| __/ _ \\ '__| '_ ` _ \\| | '_ \\ / _` |\n" +
                                                     "| |  | | (_| \\__ \\ ||  __/ |  | | | | | | | | | | (_| |\n" +
                                                     "|_|  |_|\\__,_|___/\\__\\___|_|  |_| |_| |_|_|_| |_|\\__,_|\n" +
-                                                    NSpaces(15)+"By: Quinten Hutchison - Quinten@Case.edu\n\n";
+                                                    NSpaces(15) + "By: Quinten Hutchison - Quinten@Case.edu\n\n";
 
             //chars to show on the scoreboard for correct/incorrect guesses
             public static readonly char correctScoreSymbol = '+';
             public static readonly char incorrectScoreSymbol = '-';
 
-            public static readonly String instructions =    "Try to find the " + Settings.CODE_LENGTH + " digit code by entering guesses below\n" +
+            public static readonly String instructions = "Try to find the " + Settings.CODE_LENGTH + " digit code by entering guesses below\n" +
+                                                            "The code can consister of the following characters: "+String.Join("",Settings.VALID_CHARS)+"\n" +
                                                             "You have " + Settings.MAX_GUESSES + " tries to find the code.\n'" +
                                                             correctScoreSymbol + "' indicates a correct guess in the right location\n'" +
                                                             incorrectScoreSymbol + "' indicates a correct guess in the wrong location\n";
@@ -193,30 +217,38 @@ namespace Mastermind
             /** some helper properties to clear up some math **/
 
             //the current line of the scoreboard
-            public static int ScoreboardLine {
-                get {
-                    //12 = height of the title + header row + instructions
-                    return 12 + guesses;
+            public static int ScoreboardLine
+            {
+                get
+                {
+                    //13 = height of the title + header row + instructions
+                    return 13 + guesses;
                 }
             }
 
             //Where the user will be prompted for input
-            public static int PromptLine {
-                get {
+            public static int PromptLine
+            {
+                get
+                {
                     return ScoreboardLine + 4;
                 }
             }
 
             //Where errors will be displayed
-            public static int ErrorLine {
-                get {
+            public static int ErrorLine
+            {
+                get
+                {
                     return PromptLine - 1;
                 }
             }
 
             //the scoreboard text showing the guess, and the score for that guess
-            public static string ScoreBoardText {
-                get {
+            public static string ScoreBoardText
+            {
+                get
+                {
                     return "\r" +
                             currentGuess +
                             //add spaces to line it up with the header
@@ -231,23 +263,27 @@ namespace Mastermind
             /** Some helper methods **/
 
             //clears the specified line in the console
-            public static void ClearConsoleLine(int lineNumber) {
+            public static void ClearConsoleLine(int lineNumber)
+            {
                 Console.SetCursorPosition(0, lineNumber);
                 Console.Write(NSpaces(Console.WindowWidth));
                 Console.SetCursorPosition(0, lineNumber);
             }
 
             //write the given message to the console in the given color
-            public static void WriteInColor(String message, ConsoleColor color) {
+            public static void WriteInColor(String message, ConsoleColor color)
+            {
                 Console.ForegroundColor = color;
                 Console.Write(message);
                 Console.ForegroundColor = ConsoleColor.White;
             }
 
             //a helped method to return a string of a lot of spaces
-            public static String NSpaces(int numberOfSpaces) {
+            public static String NSpaces(int numberOfSpaces)
+            {
                 StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < numberOfSpaces; i++) {
+                for (int i = 0; i < numberOfSpaces; i++)
+                {
                     builder.Append(' ');
                 }
                 return builder.ToString();
@@ -259,7 +295,7 @@ namespace Mastermind
     struct Settings
     {
         //the maximum number of guesses allowed per game
-        public const int MAX_GUESSES = 5;
+        public const int MAX_GUESSES = 10;
         //the length of the code to guess
         public const int CODE_LENGTH = 4;
         //the allowed characters in the code
