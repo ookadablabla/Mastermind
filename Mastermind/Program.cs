1eb5368317d5d4efe94abbfb62395379f4db0c76
Quinten <jqh3@case.edu>
2017-11-12 20:59:37 -0500

    Added in a function to write text in color. Moved all of the line numbers into properties so that the math is much clearer. Added an error message on invalid input. Move lots of strings into their own variables so they're easier to find and edit

--- Mastermind/Program.cs
+++ Mastermind/Program.cs
@@ -7,19 +7,53 @@ namespace Mastermind
 {
     class Game
     {
+        //variables used throughout the game
         private string code;
         private int guesses;
-        private string consoleSpacing = new String(' ', Settings.CODE_LENGTH);
+        String currentGuess;
+
+        //some constants
+        private readonly String consoleSpacing = new String(' ', Settings.CODE_LENGTH);
+        private readonly String prompt = "Guess the " + Settings.CODE_LENGTH + " digit code: ";
+        private readonly String errorMessage =  "Invalid input - Please guess a " + Settings.CODE_LENGTH + 
+                                                " digit code consisting only of the characters: " + 
+                                                String.Join("",Settings.VALID_CHARS)+"\n";
+               
+        private static readonly String title =   " __  __           _                      _           _ \n" +
+                                                 "|  \\/  | __ _ ___| |_ ___ _ __ _ __ ___ (_)_ __   __| |\n" +
+                                                 "| |\\/| |/ _` / __| __/ _ \\ '__| '_ ` _ \\| | '_ \\ / _` |\n" +
+                                                 "| |  | | (_| \\__ \\ ||  __/ |  | | | | | | | | | | (_| |\n" +
+                                                 "|_|  |_|\\__,_|___/\\__\\___|_|  |_| |_| |_|_|_| |_|\\__,_|\n\n";
+
+        //some helper properties to clear up some math
+        
+        //the current line of the scoreboard
+        private int ScoreboardLine {
+            get {
+                //7 = height of the title + header row
+                return 7 + guesses;
+            }
+        }
+
+        //Where the user will be prompted for input
+        private int PromptLine {
+            get {
+                return ScoreboardLine + 3;
+            }
+        }
+
+        //Where errors will be displayed
+        private int ErrorLine {
+            get {
+                return PromptLine - 1;
+            }
+        }
+
 
         public static void Main(String[] args) {
-            Console.WriteLine(
-             " __  __           _                      _           _ \n"+
-             "|  \\/  | __ _ ___| |_ ___ _ __ _ __ ___ (_)_ __   __| |\n"+
-             "| |\\/| |/ _` / __| __/ _ \\ '__| '_ ` _ \\| | '_ \\ / _` |\n"+
-             "| |  | | (_| \\__ \\ ||  __/ |  | | | | | | | | | | (_| |\n"+
-             "|_|  |_|\\__,_|___/\\__\\___|_|  |_| |_| |_|_|_| |_|\\__,_|\n");
-
             Game game = new Game();
+
+            game.WriteInColor(title, ConsoleColor.Green);
             game.NewGame();
 
 
@@ -32,15 +66,12 @@ namespace Mastermind
             //initialize/reset all of the variables
             guesses = 0;
             RandomizeCode();
-            String currentGuess;
-            String prompt = "Guess the " + Settings.CODE_LENGTH + " digit code: ";
-
+            
             Console.WriteLine("Guesses:" + consoleSpacing + "Results:");
-
-
+    
             while (guesses < Settings.MAX_GUESSES) {
                 //prompt for a new guess, make room for the size of the header, and some extra spacing
-                ClearConsoleLine(7 + guesses + 3);
+                ClearConsoleLine(PromptLine);
                 Console.Write(prompt);
 
                 currentGuess = Console.ReadLine();
@@ -48,11 +79,14 @@ namespace Mastermind
                 if (IsValidGuess(currentGuess)) {
                     guesses++;
 
-                    //clear the prompt for the previous question
-                    ClearConsoleLine(7 + guesses - 1 + 3);
+                    //clear the prompt and error for the previous question
+                    ClearConsoleLine(ErrorLine - 1);
+                    ClearConsoleLine(PromptLine - 1);
 
                     //account for the MASTERMID text and header
-                    Console.SetCursorPosition(Console.CursorLeft, 7 + guesses);
+                    Console.SetCursorPosition(0, ScoreboardLine);
+
+
 
                     Console.WriteLine("\r" +
                                         currentGuess +
@@ -62,7 +96,8 @@ namespace Mastermind
                                         //clear the rest of the line
                                         RepeatCharacter(' ', 20));
                 } else {
-                    //print some form of error message here
+                    Console.SetCursorPosition(0, ErrorLine);
+                    WriteInColor(errorMessage, ConsoleColor.Red);
                 }
                 
 
@@ -94,11 +129,6 @@ namespace Mastermind
             code = newCode.ToString();
         }
 
-        private void WriteAt(String message, int col, int row) {
-            Console.SetCursorPosition(col, row);
-            Console.WriteLine(message);
-        }
-
         //a simple helper method to repeat a character a certain number of times
         private String RepeatCharacter(char toRepeat, int repetitions) {
             StringBuilder builder = new StringBuilder();
@@ -126,17 +156,29 @@ namespace Mastermind
             return true;
         }
 
+        //clears the specified line in the console
         private void ClearConsoleLine(int lineNumber) {
             Console.SetCursorPosition(0, lineNumber);
-            Console.Write(RepeatCharacter(' ', 50));
+            Console.Write(RepeatCharacter(' ', 100));
             Console.SetCursorPosition(0, lineNumber);
+        }
+
+        //write the given message to the console in the given color
+        private void WriteInColor(String message, ConsoleColor color) {
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 
+    //a class to hold the game settings
     static class Settings
     {
+        //the maximum number of guesses allowed per game
         public const int MAX_GUESSES = 5;
+        //the length of the code to guess
         public const int CODE_LENGTH = 4;
+        //the allowed characters in the code
         public static readonly char[] VALID_CHARS = { '1', '2', '3', '4', '5', '6' };
     }
 }
