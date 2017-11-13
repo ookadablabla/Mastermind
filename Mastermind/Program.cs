12f518281e5455df8a256f32c99c2ee3d4a9579b
Quinten <jqh3@case.edu>
2017-11-12 21:56:59 -0500

    Moved all of the console manipulation, and messages into a helper class UI.

--- Mastermind/Program.cs
+++ Mastermind/Program.cs
@@ -8,89 +8,36 @@ namespace Mastermind
     class Game
     {
         //variables used throughout the game
-        private string code;
-        private int guesses;
-        private Boolean userHasWon = false;
-        String currentGuess;
-
-        //some constants
-        private readonly String consoleSpacing = new String(' ', Settings.CODE_LENGTH);
-        private readonly String prompt = "Guess the " + Settings.CODE_LENGTH + " digit code: ";
-        private readonly String errorMessage =  "Invalid input - Please guess a " + Settings.CODE_LENGTH + 
-                                                " digit code consisting only of the characters: " + 
-                                                String.Join("",Settings.VALID_CHARS)+"\n";
-               
-        private static readonly String title =   " __  __           _                      _           _ \n" +
-                                                 "|  \\/  | __ _ ___| |_ ___ _ __ _ __ ___ (_)_ __   __| |\n" +
-                                                 "| |\\/| |/ _` / __| __/ _ \\ '__| '_ ` _ \\| | '_ \\ / _` |\n" +
-                                                 "| |  | | (_| \\__ \\ ||  __/ |  | | | | | | | | | | (_| |\n" +
-                                                 "|_|  |_|\\__,_|___/\\__\\___|_|  |_| |_| |_|_|_| |_|\\__,_|\n\n";
-
-        //some helper properties to clear up some math
-        
-        //the current line of the scoreboard
-        private int ScoreboardLine {
-            get {
-                //7 = height of the title + header row
-                return 7 + guesses;
-            }
-        }
-
-        //Where the user will be prompted for input
-        private int PromptLine {
-            get {
-                return ScoreboardLine + 3;
-            }
-        }
-
-        //Where errors will be displayed
-        private int ErrorLine {
-            get {
-                return PromptLine - 1;
-            }
-        }
-
-        private String ScoreBoardText {
-            get {
-                return "\r" +
-                        currentGuess +
-                        //add spaces to line it up with the header
-                        RepeatCharacter(' ', "Guesses:".Length) +
-                        ScoreFromGuess(currentGuess) +
-                        //clear the rest of the line
-                        RepeatCharacter(' ', 20);
-            }
-        }
-
+        private static string code;
+        private static int guesses;
+        private static Boolean userHasWon = false;
+        private static String currentGuess;
 
         public static void Main(String[] args) {
-            Game game = new Game();
-
-            game.WriteInColor(title, ConsoleColor.Green);
-            game.NewGame();
-
-
+            NewGame();
 
             //wait for a keypress before closing
             Console.ReadKey();
         }
 
-        private void NewGame() {
+        //resets the variables and starts a new game of Mastermind
+        private static void NewGame() {
             //initialize/reset all of the variables
             guesses = 0;
             RandomizeCode();
             userHasWon = false;
 
             //Add the header for the scoreboard
-            Console.SetCursorPosition(0, ScoreboardLine - 1);
-            Console.WriteLine("Guesses:" + consoleSpacing + "Results:");
+            Console.SetCursorPosition(0, 0);
+            UI.WriteInColor(UI.title, ConsoleColor.Green);
+            Console.WriteLine("Guesses:" + UI.consoleSpacing + "Results:");
             
 
             //keep prompting the user for a guess
             while (!userHasWon && guesses < Settings.MAX_GUESSES) {
                 //prompt for a new guess, make room for the size of the header, and some extra spacing
-                ClearConsoleLine(PromptLine);
-                Console.Write(prompt);
+                UI.ClearConsoleLine(UI.PromptLine);
+                Console.Write(UI.promptMessage);
 
                 currentGuess = Console.ReadLine();
 
@@ -100,24 +47,25 @@ namespace Mastermind
                     guesses++;
 
                     //clear the prompt and error for the previous question
-                    ClearConsoleLine(ErrorLine - 1);
-                    ClearConsoleLine(PromptLine - 1);
+                    UI.ClearConsoleLine(UI.ErrorLine - 1);
+                    UI.ClearConsoleLine(UI.PromptLine - 1);
                     
                     //add the text to the scoreboard
-                    Console.SetCursorPosition(0, ScoreboardLine);
-                    Console.WriteLine(ScoreBoardText);
+                    Console.SetCursorPosition(0, UI.ScoreboardLine);
+                    Console.WriteLine(UI.ScoreBoardText);
 
                     //check if the user won
                     userHasWon = currentGuess.Equals(code);
 
                 //otherwise write an error
                 } else {
-                    Console.SetCursorPosition(0, ErrorLine);
-                    WriteInColor(errorMessage, ConsoleColor.Red);
+                    Console.SetCursorPosition(0, UI.ErrorLine);
+                    UI.WriteInColor(UI.errorMessage, ConsoleColor.Red);
                 }
             }
 
-            ClearConsoleLine(PromptLine);
+            //clear out the current message, and write the win/lose message
+            UI.ClearConsoleLine(UI.PromptLine);
             if(userHasWon) {
                 Console.WriteLine("You solved it!");
             } else {
@@ -127,7 +75,7 @@ namespace Mastermind
         }
 
         //set the current solution to be a new random valid code
-        private void RandomizeCode() {
+        private static void RandomizeCode() {
             StringBuilder newCode = new StringBuilder();
             Random random = new Random();
 
@@ -139,17 +87,8 @@ namespace Mastermind
             code = newCode.ToString();
         }
 
-        //a simple helper method to repeat a character a certain number of times
-        private String RepeatCharacter(char toRepeat, int repetitions) {
-            StringBuilder builder = new StringBuilder();
-            for(int i=0; i< repetitions; i++) {
-                builder.Append(toRepeat);
-            }
-            return builder.ToString();
-        }
-
         //checks to make sure the guess only contains valid characters
-        private bool IsValidGuess(String guess)  {
+        private static bool IsValidGuess(String guess)  {
             //check the length
             if(guess.Length != Settings.CODE_LENGTH) {
                 return false;
@@ -167,7 +106,7 @@ namespace Mastermind
         }
 
         //checks the guess against the correct code, and returns the resulting score
-        private String ScoreFromGuess(String guess) {
+        private static String ScoreFromGuess(String guess) {
             List<char> matching = new List<char>();
             List<char> wrongPosition = new List<char>();
             char currentChar;
@@ -200,25 +139,92 @@ namespace Mastermind
 
             return score.ToString();
 
-        }
+        }        
 
-        //clears the specified line in the console
-        private void ClearConsoleLine(int lineNumber) {
-            Console.SetCursorPosition(0, lineNumber);
-            Console.Write(RepeatCharacter(' ', 100));
-            Console.SetCursorPosition(0, lineNumber);
-        }
+        private static class UI
+        {
+            /** String constants to make the rest of the code more readable **/
 
-        //write the given message to the console in the given color
-        private void WriteInColor(String message, ConsoleColor color) {
-            Console.ForegroundColor = color;
-            Console.Write(message);
-            Console.ForegroundColor = ConsoleColor.White;
+            public static readonly String consoleSpacing = new String(' ', Settings.CODE_LENGTH);
+            public static readonly String promptMessage = "Guess the " + Settings.CODE_LENGTH + " digit code: ";
+            public static readonly String errorMessage = "Invalid input - Please guess a " + Settings.CODE_LENGTH +
+                                                    " digit code consisting only of the characters: " +
+                                                    String.Join("", Settings.VALID_CHARS) + "\n";
+
+            public static readonly String title = " __  __           _                      _           _ \n" +
+                                                     "|  \\/  | __ _ ___| |_ ___ _ __ _ __ ___ (_)_ __   __| |\n" +
+                                                     "| |\\/| |/ _` / __| __/ _ \\ '__| '_ ` _ \\| | '_ \\ / _` |\n" +
+                                                     "| |  | | (_| \\__ \\ ||  __/ |  | | | | | | | | | | (_| |\n" +
+                                                     "|_|  |_|\\__,_|___/\\__\\___|_|  |_| |_| |_|_|_| |_|\\__,_|\n\n";
+
+
+            /** some helper properties to clear up some math **/
+
+            //the current line of the scoreboard
+            public static int ScoreboardLine {
+                get {
+                    //6 = height of the title + header row
+                    return 6 + guesses;
+                }
+            }
+
+            //Where the user will be prompted for input
+            public static int PromptLine {
+                get {
+                    return ScoreboardLine + 3;
+                }
+            }
+
+            //Where errors will be displayed
+            public static int ErrorLine {
+                get {
+                    return PromptLine - 1;
+                }
+            }
+
+
+            public static String ScoreBoardText {
+                get {
+                    return "\r" +
+                            currentGuess +
+                            //add spaces to line it up with the header
+                            UI.RepeatCharacter(' ', "Guesses:".Length) +
+                            ScoreFromGuess(currentGuess) +
+                            //clear the rest of the line
+                            UI.RepeatCharacter(' ', 20);
+                }
+            }
+
+
+            /** Some helper methods **/
+
+            //clears the specified line in the console
+            public static void ClearConsoleLine(int lineNumber) {
+                Console.SetCursorPosition(0, lineNumber);
+                Console.Write(RepeatCharacter(' ', 100));
+                Console.SetCursorPosition(0, lineNumber);
+            }
+
+            //write the given message to the console in the given color
+            public static void WriteInColor(String message, ConsoleColor color) {
+                Console.ForegroundColor = color;
+                Console.Write(message);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            //a simple helper method to repeat a character a certain number of times
+            public static String RepeatCharacter(char toRepeat, int repetitions) {
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < repetitions; i++) {
+                    builder.Append(toRepeat);
+                }
+                return builder.ToString();
+            }
         }
     }
 
     //a class to hold the game settings
-    static class Settings
+    struct Settings
     {
         //the maximum number of guesses allowed per game
         public const int MAX_GUESSES = 5;
